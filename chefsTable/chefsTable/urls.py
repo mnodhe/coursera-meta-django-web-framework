1e6b413ff3f472e7182a2b00eed8ea5c450eba5e
mnodhe <mnodhe@gmail.com>
2023-01-08 12:22:26 +0330

    add limmon app to project

--- chefsTable/chefsTable/urls.py
+++ chefsTable/chefsTable/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from littleLemon import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',view=views.home,name='home'),
 ]
