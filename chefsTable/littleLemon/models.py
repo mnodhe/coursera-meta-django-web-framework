11d1922d020460767fcad36ab5a46b03c1d26eb4
mnodhe <mnodhe@gmail.com>
2023-01-10 11:22:09 +0330

    one to many relation in menu and menu category

--- chefsTable/littleLemon/models.py
+++ chefsTable/littleLemon/models.py
@@ -3,17 +3,27 @@ from django.db import models
 
 # Create your models here.
 
+# let's add menu category to learn foreign key
+
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    # category = models.CharField(max_length=100)
+    # we need to change it to use foreign key
+    category_id = models.ForeignKey(
+        MenuCategory, on_delete=models.PROTECT, default=None, related_name="category_name")
     price = models.IntegerField()
 
     def __str__(self) -> str:
         return self.name + ' ' + self.cuisine
-    
-class Person(models.Model): 
-    person_name = models.CharField(max_length=20) 
-    person_age = models.IntegerField() 
-    email = models.EmailField() 
-    phone = models.CharField(max_length=20) 
+
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=20)
+    person_age = models.IntegerField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
