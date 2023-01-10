d2e2f02da31ece33c5c67cead8473c8234b56897
mnodhe <mnodhe@gmail.com>
2023-01-10 10:50:11 +0330

    working with migrations

--- chefsTable/littleLemon/models.py
+++ chefsTable/littleLemon/models.py
@@ -6,8 +6,14 @@ from django.db import models
 
 class Menu(models.Model):
     name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     price = models.IntegerField()
 
     def __str__(self) -> str:
         return self.name + ' ' + self.cuisine
+    
+class Person(models.Model): 
+    person_name = models.CharField(max_length=20) 
+    person_age = models.IntegerField() 
+    email = models.EmailField() 
+    phone = models.CharField(max_length=20) 
